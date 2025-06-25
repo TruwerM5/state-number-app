03877c4e37f88e9b5f4831e57408ffd3687527f7
halimov <halimov@infomatika.ru>
2025-06-25 22:25:50 +0300

    feat: redeploy

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,111 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import NumberForm from './components/NumberForm.vue';
+/*
+в 888 ор 16
+а 009 уе 716
+а 050 ао 16
+у 716 рр 716
+р 716 ак 716
+у 716 во 716
+у 001 нм 116
+у 001 во 116
+к 001 ас 716
+р 999 но 116
+о 999 рс 116
+у 999 нт 116
+в 999 хс 716
+в 999 рх 716
+е 999 на 716
+т 888 тр 116
+у 888 мо 116
+в 888 хм 716
+т 777 мр 116
+а 777 ме 116
+в 555 тр 116
+р 444 ме 116
+е 444 тс 716
+*/
+const textareaModel = ref('');
+const isTextareaOpened = ref(true);
+const numbers = ref<string[]>([]);
+
+function fillData () {
+  if(textareaModel.value.length === 0) {
+    return;
+  }
+  numbers.value = textareaModel.value.split('\n');
+  isTextareaOpened.value = false;
+}
+
 </script>
 
 <template>
-  <NumberForm />
+  <div class="wrapper">
+    <div v-if="isTextareaOpened" class="textarea-wrapper">
+      <textarea 
+        v-model="textareaModel" 
+        name="data" 
+        id="data" 
+        class="textarea"
+        placeholder="Список номеров. Каждый номер на отдельной строке.в 888 ор 16"
+      ></textarea>
+      <button @click="fillData" class="apply-button">Готово</button>
+    </div>
+    <div class="numbers">
+      <NumberForm v-for="number in numbers" :key="number" :number="number" />
+    </div>
+    
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style scoped lang="sass">
+.wrapper
+  width: 100%
+
+.textarea-wrapper
+  position: fixed
+  inset: 0
+  width: 100vw
+  height: 100vh
+  display: flex
+  flex-direction: column
+  gap: 15px
+  justify-content: center
+  align-items: center
+  background-color: rgba(0, 0, 0, 0.4)
+  backdrop-filter: blur(10px)
+  z-index: 10
+
+.textarea
+  display: block
+  height: 80%
+  width: 90%
+  font-size: 16px
+  padding: 20px
+  font-family: sans-serif
+  resize: none
+  border: 2px solid #000
+  border-radius: 10px
+
+.numbers
+  padding: 30px 0
+  display: grid
+  grid-template-columns: 1fr
+  justify-content: center
+  align-items: center
+  gap: 20px
+  @media screen and (min-width: 960px)
+    grid-template-columns: 1fr 1fr
+  @media screen and (min-width: 1568px)
+    grid-template-columns: 1fr 1fr 1fr
+  
+.apply-button
+  padding: 10px 20px
+  color: #fff
+  background-color: #000
+  border: none
+  font-size: 16px
+  border-radius: 5px
+  cursor: pointer
 </style>
--- src/components/NumberForm.vue
+++ src/components/NumberForm.vue
@@ -1,133 +1,50 @@
 
 <script lang="ts" setup>
-import { ref, reactive } from 'vue';
+import { computed } from 'vue';
 // @ts-ignore
 import Flag from './Flag.vue';
-const region_input = ref<any>();
-const id_input = ref<any>();
-const error_message = ref('');
 
-const state = reactive({
-    id_value: '',
-    region_value: '',
-    numbers: '',
-    characters: '',
-    allowed_characters: 'АВЕКМНОРСТУХ1234567890'
+const props = defineProps<{
+    number: string;
+}>();
+
+const getFirstSymbol = computed(() => {
+    return props.number.length ? props.number[0].toUpperCase() : null;
+});
+
+const getNumbers = computed(() => {
+    return props.number.slice(2, 5).toUpperCase();
+});
+
+const getLastSymbols = computed(() => {
+    return props.number.slice(6, 8).toUpperCase();
+});
+
+const getRegion = computed(() => {
+    return props.number.slice(9).toUpperCase();
 });
 
 
 
-function uppercaseNumberId() {
-    state.id_value = state.id_value.toUpperCase(); 
-}
-
-function onKeyUp () {
-    // let value = e.target.value.toUpperCase();
-    uppercaseNumberId();
-    
-    let filtered_2 = [];
-    let filtered = state.id_value.split('')
-    .filter(char => {
-        if(state.allowed_characters.includes(char)) {
-            return true
-        } else {
-            error_message.value = 'Введен неверный символ. Разрешены следующие символы АВЕКМНОРСТУХ1234567890'
-            return false;
-        }
-        
-    })
-    // .filter(char => {
-    //     if(
-    //     (symbols.includes(char[0]) || !char[0]) && 
-    //     () &&
-    //     (Number(char[2]) || !char[2]) &&
-    //     (Number(char[3] || !char[3])) &&
-    //     (symbols.includes(char[4]) || !char[4]) &&
-    //     (symbols.includes(char[5]) || !char[5])
-    //     ){
-            
-    //         return true;
-    //     } else {
-    //         console.log('Dismatch',numberStore.id_value.length < 2);
-    //         console.log(char[1])
-    //         return false;
-    //     }
-        
-    // })
-
-    if(filtered.length > 0) {
-        if(!Number(filtered[0]) && typeof filtered[0] == 'string' && Number(filtered[0]) != 0) {
-            filtered_2.push(filtered[0]);
-        }
-        if(filtered.length > 1) {
-            if(Number(filtered[1]) > -1) {
-                filtered_2.push(filtered[1])
-            }
-            if(filtered.length > 2) {
-                if(Number(filtered[2]) > -1) {
-                    filtered_2.push(filtered[2]);
-                }
-                if(filtered.length > 3) {
-                    if(Number(filtered[3]) > -1) {
-                        filtered_2.push(filtered[3])
-                    }
-                    if(filtered.length > 4) {
-                        if(!Number(filtered[4]) && typeof filtered[4] == 'string' && Number(filtered[4]) != 0) {
-                            filtered_2.push(filtered[4])
-                        }
-                        if(filtered.length > 5) {
-                            if(!Number(filtered[5]) && typeof filtered[5] == 'string' && Number(filtered[5]) != 0) {
-                            filtered_2.push(filtered[5])
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    filtered = filtered_2;
-    state.id_value = filtered_2.join('');
-    if(state.id_value.length == 6) {
-        region_input.value.focus();
-    }
-
-    
-}
-
-
-function onRegionKeyUp() {
-    let filtered = state.region_value
-    .split('')
-    .filter(char => Number(char) > -1)
-    .join('');
-    state.region_value = filtered;
-}
-
 
 </script>
 <template>
-    <div class="car-number-form" @click="id_input.focus()">
-        
+    <div class="car-number-form">
         <div class="car-number-form__id">
-            <input type="text" name="number_id" id="number_id" 
-            maxlength="6" minlength="6"
-            placeholder="М707РН"
-            class="car-number-form__input car-number-form__id-input"
-            v-model="state.id_value"
-            @keyup="onKeyUp"
-            ref="id_input"
-            pattern="[АВЕКМНОРСТУХ1234567890]*">
+           <span class="symbol">
+            {{ getFirstSymbol }}
+           </span>
+           <span class="symbol">
+            {{ getNumbers }}
+           </span>
+           <span class="symbol">
+            {{ getLastSymbols }}
+           </span>
         </div>
         <div class="car-number-form__region">
-            <input type="text" name="number_region" id="number_region" 
-            minlength="2" maxlength="3"
-            placeholder="716"
-            v-model="state.region_value"
-            ref="region_input"
-            class="car-number-form__input car-number-form__region-input"
-            style="top: 25px;"
-            @keyup="onRegionKeyUp">
+            <span class="symbol">
+                {{ getRegion }}
+            </span>
             <div class="car-number-form__region-bottom">
                 <span class="car-number-form__region-text">
                     RUS
@@ -147,19 +64,10 @@ function onRegionKeyUp() {
     border: 6px solid #000
     width: 100%
     border-radius: 14px
-    max-width: 425px
+    max-width: 450px
     margin: 0 auto
+    max-height: 100px
     font-family: RoadNumbers
-    // &::before,
-    // &::after
-    //     position: absolute
-    //     top: 50%
-    //     transform: translateY(-50%)
-    //     content: ""
-    //     width: 5px
-    //     height: 5px
-    //     border-radius: 50%
-    //     border: 2px solid #000
     &::before
         left: 5px
     &::after
@@ -167,45 +75,33 @@ function onRegionKeyUp() {
     &__input
         border-radius: 8px
         text-align: center
-        letter-spacing: 2px
-        font-family: RoadNumbers, sans-serif
         letter-spacing: 6px
     &__id
         position: relative
-        height: 90px
-    &__input
-        position: absolute
-        top: -13px
+        height: 100%
+        display: flex
+        font-size: 75px
+        align-items: center
+        justify-content: center
+        letter-spacing: 6px
         height: 80px
-        z-index: 0
-        z-index: -1
-    &__id-input
-        padding-block: 5px
-        padding-left: 10px
-        font-size: 80px
+        @media screen and (min-width: 1568px)
+            font-size: 80px
+            letter-spacing: 8px
+            padding-bottom: 10px
     &__region
         position: relative
+        display: flex
+        align-items: center
+        justify-content: center
         border-left: 4px solid #000
-        font-family: sans-serif
+        font-family: RoadNumbers
         line-height: 1.8
-        &::before,
-        &::after
-            position: absolute
-            display: block
-            content: ""
-            left: -2px
-            transform: translateX(-50%)
-            width: 0
-            height: 0
-            border-left: 6px solid transparent
-            border-right: 6px solid transparent
-        &::before
-            top: 0
-            border-top: 6px solid black
-        &::after
-            bottom: 0
-            border-bottom: 6px solid black
-            
+        font-size: 55px
+        height: 80px
+        padding-bottom: 40px
+        .symbol
+            letter-spacing: 4px
     &__region-input
         display: block
         position: absolute
